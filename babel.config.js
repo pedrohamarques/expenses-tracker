452fbed5031776a9e35c88754680bab2ed0b7651
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-02 00:03:31 -0300

    feat: add format date utils

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
             "@routes": "./src/routes",
             "@constants": "./src/constants",
             "@components": "./src/components",
+            "@utils": "./src/utils",
           },
         },
       ],
---
+++ src/utils/date.ts
@@ -0,0 +1,31 @@
+const MONTHS = {
+  January: 0,
+  February: 1,
+  March: 2,
+  April: 3,
+  May: 4,
+  June: 5,
+  July: 6,
+  August: 7,
+  September: 8,
+  October: 9,
+  November: 10,
+  December: 11,
+};
+
+function getMonthName(month: number) {
+  const keys = Object.keys(MONTHS);
+  return keys[month];
+}
+
+function formatDate(date: string) {
+  if (date.length === 1) {
+    return "0" + date;
+  }
+
+  return date;
+}
+
+export function getFormattedDate(date: Date) {
+  return `${formatDate(String(date.getDate()))} / ${getMonthName(date.getMonth())} / ${date.getFullYear()}`;
+}
