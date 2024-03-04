3fc00a103206a96f2435518735198a6422f11f43
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-04 18:36:42 -0300

    create error and loading component

---
+++ src/components/ui/error.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GlobalStyles } from "@constants/styles";
+import { Text, StyleSheet, View } from "react-native";
+
+type ErrorProps = {
+  message: string;
+};
+
+export function Error({ message }: ErrorProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>An error occurred!</Text>
+      <Text style={styles.text}>{message}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  text: {
+    textAlign: "center",
+    marginBottom: 8,
+    color: "white",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,4 @@
 export { IconButton } from "./icon-button";
 export { Button } from "./button";
+export { Loading } from "./loading";
+export { Error } from "./error";
